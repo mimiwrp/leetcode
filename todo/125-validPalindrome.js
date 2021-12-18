57973a1f2be67bcaf9d44ee6be91ce95122c54e0
William Owen <william.owen.bsc@gmail.com>
2021-12-17 17:31:15 -0800

    Work on palindrome

--- todo/125-validPalindrome.js
+++ todo/125-validPalindrome.js
@@ -3,9 +3,24 @@
 //  * @return {boolean}
 //  */
  var isPalindrome = function(s) {
-    
+   console.log(s.trim(), s.trim().length);
+  //  base case
+  // if(s === ' ' ){
+  //   return true
+  // }
+  if(s.trim().length === 0 ){
+    return true
+  }
+    // take out all white space, anything not a-z (regex)  /a-z/.match
+    const forward = s.match(/[A-Za-z]/g).join('').toLowerCase()
+    const reverse = s.match(/[A-Za-z]/g).reverse().join('').toLowerCase()
+    // original string and compare with reverse of that string
+    if(forward === reverse) return true
+    return false
+    //  if they are equal to eachother => true
+    // else => false
 };
 
-isPalindrome("A man, a plan, a canal: Panama")
-isPalindrome("race a car")
-isPalindrome(" ")
+// console.log(isPalindrome("A man, a plan, a canal: Panama"))
+// console.log(isPalindrome("race a car"))
+console.log(isPalindrome("      "))
